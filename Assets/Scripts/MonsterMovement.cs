0a3a6e383832124d96cce9de8eaa79684fb386a2
unknown <Jwidjaja07@gmail.com>
2020-11-19 16:29:19 -0800

    Added attack animation, monster/player movement

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script handles movement of enemy objects
+public class MonsterMovement : MonoBehaviour
+{
+    //Initializing Variables
+    public float monsterSpeed;
+    public Rigidbody2D rigidbody2D;
+    public float followRange = 1.5f;
+    private TargetController tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+
+    // Update is called once per frame
+    //Want enemy to move towards player every frame
+    void Update()
+    {
+        move();
+    }
+
+    //Helper function that handles whether to move the monster/enemy left or right towards the player
+    //Most likely will be updated in later versions
+    void move()
+    {
+        tmp = GetComponent<TargetController>();
+        bool has_target = tmp.target_locked;
+        
+        float directionVal = 0f;
+        
+        //If target is found, we compute distance and then check what it is.
+        //Sets directionVal to either -1 or 1, we use this to determine going left or right.
+        //If no target, ddon't move.
+        
+        if (has_target)
+        {
+            float comp_dist = tmp.enemy_pos.x - transform.position.x;
+            
+
+            if (comp_dist > 1f)
+                directionVal = 1.0f;
+            else if (comp_dist < 1f)
+                    directionVal = -1.0f;
+
+            Vector2 moveDir = new Vector2(directionVal * monsterSpeed, rigidbody2D.velocity.y);
+            rigidbody2D.velocity = moveDir;
+        }
+
+        else
+        {
+            rigidbody2D.velocity = new Vector2(0.0F, rigidbody2D.velocity.y);
+        }
+    }   
+
+    //Old code use in move() function. May be useful later?
+
+    /*float comp_dist = Mathf.Abs(tmp.enemy_pos.x - transform.position.x);
+
+    if (comp_dist >= 1.5F && (tmp.enemy_pos.x - transform.position.x) > 0)
+        directionVal = 1.0f;
+    else if ((tmp.enemy_pos.x - transform.position.x) < 0)
+        directionVal = -1.0f;
+
+    Vector2 moveDir = new Vector2(directionVal * monsterSpeed, rigidbody2D.velocity.y);
+
+    rigidbody2D.velocity = moveDir;*/
+
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This script handles movement and actions of the player.
+public class playerMovement : MonoBehaviour
+{
+    //Initializing and defining variables
+
+    public float walkSpeed; //Default: 4f? Subject to change
+    private SpriteRenderer playerSpriteRenderer;
+    private Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Assigned components to variables
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        // "A" and "D" movement keys to move left and right (may remove later)
+        // "J" for attack
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * walkSpeed, Space.World);
+            playerSpriteRenderer.flipX = true;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * walkSpeed, Space.World);
+            playerSpriteRenderer.flipX = false;
+        }
+
+        if (Input.GetKeyDown("j"))
+        {
+            playerAnimator.SetTrigger("Attack");
+            //Debug.Log(" DOING ANIMATION");
+    
+        }
+
+        
+    }
+}
