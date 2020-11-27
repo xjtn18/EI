ec420b6304665628abbec6b7dd25de462bf11c81
unknown <Jwidjaja07@gmail.com>
2020-11-27 15:19:28 -0800

    Added GameOver UI and SceneFader

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Game manager script is the overseer of the game. Checks to see whether or not the game is over.
+public class GameManager : MonoBehaviour
+{
+    //Initializing variables
+    //Right now just for GameOver
+    public static bool gameOverStatus;
+    public GameObject gameOverUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverStatus = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOverStatus == true)
+        {
+            return;
+        }
+            
+        //Simulates being hit. Calls GameOver() if health is 0.
+        if (Input.GetKeyDown("g"))
+        {
+            PlayerInfo.health--;
+            //Debug statement
+            Debug.Log(PlayerInfo.health.ToString());
+            if (PlayerInfo.health == 0)
+            {
+                GameOver();
+            }
+        }
+    }
+
+
+
+    //GameOver functions triggers the UI to become active.
+    void GameOver()
+    {
+        gameOverStatus = true;
+        gameOverUI.SetActive(true);
+        //Debug statement
+        Debug.Log("Game Ended");
+
+    }
+}
+
+
+    
---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Contains information relevant to the player, such as stats, equipment, etc.
+public class PlayerInfo : MonoBehaviour
+{
+    //Initializing variables
+    public static int health;
+    public int startingHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
