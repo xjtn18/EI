6197472fc0e19b72e79b902e0dc1502e3d48bbfe
broffmanS <sbroffma@uci.edu>
2020-12-03 16:11:02 -0800

    Scripts for play and exit button added

---
+++ Assets/Scripts/ExitButtonBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonBehaviourScript : MonoBehaviour
+{
+    //Exit game method
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
