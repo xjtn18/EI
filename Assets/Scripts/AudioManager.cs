31a9f91c1c9a5a42e08b0e84f71f097f9f032266
Jacob N <xjtn18@gmail.com>
2020-12-04 18:24:46 -0800

    replay button works, add test music, increase monster speed range

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -15,11 +15,13 @@ public class AudioManager {
 
 	// Start is called before the first frame update
 	static AudioManager() {
-		channels = GameObject.Find("MainAudio").GetComponents<AudioSource>();
+		GameObject mainAudio = GameObject.Find("MainAudio");
+		channels = mainAudio.GetComponents<AudioSource>();
 		
 		// set channel 0 for game music
 		//PlaySoundOn("hapi_tomi", 0, 0.05f, true); // music
 		PlaySoundOn("outdoor_ambience", 1, 0.2f, true);
+		PlaySoundOn("major_theme", 1, 0.2f, true);
 
 	}
 
