7db840b1bf1b03dcfcf9adcd9d145202fb67f3f6
broffmanS <sbroffma@uci.edu>
2020-12-05 16:05:57 -0800

    Added joshuaBranch asset folder manually

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //Contains information relevant to the player, such as stats, equipment, etc.
 public class PlayerInfo : MonoBehaviour
@@ -8,7 +9,9 @@ public class PlayerInfo : MonoBehaviour
     //Initializing variables
     public static int health;
     public int startingHealth;
-
+    public Image[] heartsArray;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +22,29 @@ public class PlayerInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Handles the healthUI display. Chooses whether to display fullHeart sprite or emptyHeart
+        //depending on player health.
+        for (int i = 0; i < heartsArray.Length; i++)
+        {
+            if (i < health)
+            {
+                heartsArray[i].sprite = fullHeart;
+            }
+            else
+            {
+                heartsArray[i].sprite = emptyHeart;
+            }
+
+            if (i < startingHealth)
+            {
+                heartsArray[i].enabled = true;
+            }
+            else
+            {
+                heartsArray[i].enabled = false;
+            }
+        }
     }
 
 }
+
