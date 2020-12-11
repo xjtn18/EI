3c0ab4fa07019ed57501420332538559ec73c759
unknown <Jwidjaja07@gmail.com>
2020-12-11 02:05:31 -0800

    Merged with sleepo's branch and fixes conflict

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script handles movement of enemy objects
+public class MonsterMovement : MonoBehaviour
+{
+    //Initializing Variables
+    public Monster monster;
+    public new Rigidbody2D rigidbody2D;
+    public float followRange = 1.5f;
+    public float monsterSpeed;
+    public float minMonsterSpeed = 1.5f;
+    public float maxMonsterSpeed = 4.5f;
+    public float addedSpeed = 1.0f;
+
+    private TargetController tmp;
+    private Animator monsterAnimator;
+    private bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterAnimator = GetComponent<Animator>();
+        tmp = GetComponent<TargetController>();
+        monsterSpeed = Random.Range(minMonsterSpeed, maxMonsterSpeed);
+		isMoving = true;
+    }
+
+
+    // Update is called once per frame
+    //Want enemy to move towards player every frame
+    void Update()
+    {
+		if (isMoving){
+			move();
+		}
+        else
+        {
+            Debug.Log("Got here");
+        }
+    }
+
+    //Helper function that handles whether to move the monster/enemy left or right towards the player
+    //Most likely will be updated in later versions
+    public void move()
+    {
+        bool has_target = tmp.target_locked;
+        float directionVal = 0f;
+
+        //If target is found, we compute distance and then check what it is.
+        //Sets directionVal to either -1 or 1, we use this to determine going left or right.
+        //If no target, ddon't move.
+        if (has_target)
+        {
+            float comp_dist = tmp.enemy_pos.x - transform.position.x;
+
+            if (Mathf.Abs(comp_dist) <= followRange)
+            {
+                monsterAnimator.SetTrigger("IdleFromWalk");
+                rigidbody2D.velocity = new Vector2(0.0F, rigidbody2D.velocity.y);
+                return;
+            }
+
+            if (comp_dist > 1f)
+                directionVal = 1.0f;
+            else if (comp_dist < 1f)
+                directionVal = -1.0f;
+
+            //Set the correct walking animation here, then move in the correct direction.
+            SetAnimation(monster.type);
+            Vector2 moveDir = new Vector2(directionVal * monsterSpeed, rigidbody2D.velocity.y);
+            rigidbody2D.velocity = moveDir;
+        }
+
+        else
+        {
+            rigidbody2D.velocity = new Vector2(0.0F, rigidbody2D.velocity.y);
+        }
+    }
+
+    //Helper functions
+    
+    //Adds preset value to both min and max monster speed to make the game more difficult.
+    public void updateSpeed()
+    {
+        minMonsterSpeed += addedSpeed;
+        maxMonsterSpeed += addedSpeed;
+    } 
+
+	public void setMoving(bool newValue)
+    {
+		isMoving = newValue;
+	}
+
+    //Sets animation based on monster type
+    public void SetAnimation(int monsterType)
+    {
+        if (monsterType == 0)
+        {
+            monsterAnimator.SetTrigger("Walk0");
+        }
+        else if (monsterType == 1)
+        {
+            monsterAnimator.SetTrigger("Walk1");
+        }
+        else if (monsterType == 2)
+        {
+            monsterAnimator.SetTrigger("Walk2");
+        }
+        else if (monsterType == 3)
+        {
+            monsterAnimator.SetTrigger("Walk3");
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MonsterSpawner : MonoBehaviour
+{
+    //May want to implement using Corountine method
+    public Monster monster;
+    public SpriteRenderer monsterSprite;
+    public Sprite[] monsterSpriteArray;
+    public WaveController waveController;
+
+    private float waveSpawnChance = 0.8f;
+    private readonly string[] monsterSounds = new string[] { "skelly", "slime", "goblin_run", "slime" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterSprite = monster.GetComponent<SpriteRenderer>();
+        waveController = waveController.GetComponent<WaveController>();
+     
+    }
+    // Update is called once per frame
+    //Determines whether or not to spawn monsters
+    //Selects a random sprite from monsterSpriteArray, chooses that sprite to display
+    void Update()
+    {
+        if (PlayerInfo.health <= 0)
+        {
+            return;
+        }
+        {
+            if (waveController.getMonstersAlive() <= 1 && GameObject.FindGameObjectsWithTag("Player").Length == 1)
+            {
+                float randomNumber = Random.Range(0, 1.0f);
+                if (randomNumber >= waveSpawnChance)
+                {
+                    waveController.startWave();
+                    StartCoroutine(spawnMonsters(waveController.getMonstersAlive()));
+                }
+            }
+        }
+    }
+
+
+    IEnumerator spawnMonsters(float numMonsters)
+    {
+
+        for (int i = 0; i < numMonsters; i++)
+        {
+            DetermineMonster();
+            // wait random short amount of time to prevent monsters from being in perfect sync
+            float randomWaitTime = Random.Range(0f, 1f);
+            yield return new WaitForSeconds(randomWaitTime);
+
+            GameObject tmp = GameObject.FindGameObjectsWithTag("Player")[0];
+            Vector2 pos = new Vector2(tmp.transform.position.x, monster.yPos);
+            float screenRatio = (float)Screen.width / (float)Screen.height;
+            float trueXWidth = Camera.main.orthographicSize * screenRatio;
+
+            if (i % 2 == 0)
+            {
+                pos.x -= trueXWidth + i * 2;
+                monsterSprite.flipX = false;
+                var monsterInstance = Instantiate(monster, pos, new Quaternion(0, 0, 0, 1));
+            }
+            else
+            {
+                pos.x += trueXWidth + i * 2;
+                monsterSprite.flipX = true;
+                var monsterInstance = Instantiate(monster, pos, new Quaternion(0, 0, 0, 1));
+            }
+        }
+    }
+
+    public float DetermineYPosition(int monsterType)
+    {
+        if (monsterType == 0)
+        {
+            return -2.5f;
+        }
+
+        else if (monsterType == 1)
+        {
+            return -2.7f;
+        }
+
+        else if (monsterType == 2)
+        {
+            return -2.3f;
+        }
+
+        else if (monsterType == 3)
+        {
+            return -2.5f;
+        }
+
+        else
+        {
+            return -2.5f;
+        }
+    }
+
+    private void DetermineMonster()
+    {
+        int randomNum = Random.Range(0, monsterSpriteArray.Length);
+        monster.GetComponent<SpriteRenderer>().sprite = monsterSpriteArray[randomNum];
+        monster.type = randomNum;
+        monster.yPos = DetermineYPosition(randomNum);
+        monster.GetComponent<AudioSource>().clip = AudioManager.GetSound(monsterSounds[randomNum]);
+    }
+}
+
+
